1997f7313faaa21e4498e767a340282778498ef8
Tomáš <hons.t.m@seznam.cz>
2021-04-04 14:36:13 +0200

    Add test for SimpleArgument's init and use proper assert statements
    Method assertCountEqual is much superior to compare_iterables_content written in hand.

--- tests/test_symbolic_argument.py
+++ tests/test_symbolic_argument.py
@@ -6,12 +6,6 @@ from neads.symbolic_argument import *
 
 class TestSimpleArgument(unittest.TestCase):
 
-    def compare_iterables_content(self, expected_iterable, actual_iterable):
-        """Test content of iterables while abstracting their actual type."""
-        exp_expanded = list(expected_iterable)
-        act_expanded = list(actual_iterable)
-        self.assertEqual(exp_expanded, act_expanded)
-
     def setUp(self) -> None:
         self.INT_VALUE = 100
         self.symbol_a = Symbol('a')
@@ -19,6 +13,13 @@ class TestSimpleArgument(unittest.TestCase):
         self.value = Value(self.INT_VALUE)
         self.simple_symb_a = SimpleArgument(self.symbol_a)
         self.simple_val = SimpleArgument(self.value)
+
+    def test_init_with_wrong_type(self):
+        self.assertRaises(
+            TypeError,
+            SimpleArgument,
+            'this is not Symbol nor Value'
+        )
 
     def test_get_actual_argument_value_with_symbol(self):
         self.assertRaises(
@@ -33,12 +34,12 @@ class TestSimpleArgument(unittest.TestCase):
     def test_get_symbols_with_symbol(self):
         expected_iterable = [self.symbol_a]
         actual_iterable = self.simple_symb_a.get_symbols()
-        self.compare_iterables_content(expected_iterable, actual_iterable)
+        self.assertCountEqual(expected_iterable, actual_iterable)
 
     def test_get_symbols_with_value(self):
         expected_iterable = []
         actual_iterable = self.simple_val.get_symbols()
-        self.compare_iterables_content(expected_iterable, actual_iterable)
+        self.assertCountEqual(expected_iterable, actual_iterable)
 
     def test_substitute_value_incorrect_symbol(self):
         not_symbol = 'this string is not a symbol'
@@ -72,8 +73,7 @@ class TestSimpleArgument(unittest.TestCase):
         self.assertEqual(False, ret_val)
         # Test that content of simple_symb_a has not changed
         expected = [self.symbol_a]
-        self.compare_iterables_content(expected,
-                                       self.simple_symb_a.get_symbols())
+        self.assertCountEqual(expected, self.simple_symb_a.get_symbols())
 
     def test_substitute_value_nominal_case(self):
         ret_val = self.simple_symb_a.substitute_value(self.symbol_a, self.value)
@@ -118,15 +118,14 @@ class TestSimpleArgument(unittest.TestCase):
         self.assertEqual(False, ret_val)
         # Test that content of simple_symb_a has not changed
         expected = [self.symbol_a]
-        self.compare_iterables_content(expected,
-                                       self.simple_symb_a.get_symbols())
+        self.assertCountEqual(expected, self.simple_symb_a.get_symbols())
 
     def test_substitute_symbol_nominal_case(self):
         ret_val = self.simple_symb_a.substitute_symbol(self.symbol_a,
                                                        self.symbol_b)
 
         self.assertEqual(True, ret_val)
-        self.compare_iterables_content(
+        self.assertCountEqual(
             [self.symbol_b],
             self.simple_symb_a.get_symbols()
         )
