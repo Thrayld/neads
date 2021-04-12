d6a552cb373d0b0138fad30e095607287124178a
Tomáš <hons.t.m@seznam.cz>
2021-04-12 16:30:23 +0200

    Add Symbol and SymbolicObject tests

---
+++ tests/test_symbolic_object/test_symbol.py
@@ -0,0 +1,58 @@
+import unittest
+
+from neads.symbolic_objects import *
+
+
+class TestSymbol(unittest.TestCase):
+    def setUp(self) -> None:
+        self.symbol = Symbol()
+        self.diff_symbol = Symbol()
+
+        self.int_value = 0
+        self.value = Value(self.int_value)
+
+    def test_substitute_different_symbol(self):
+        after_subs = self.symbol.substitute(self.diff_symbol, self.value)
+
+        self.assertRaises(
+            SymbolicObjectException,
+            after_subs.get_value
+        )
+        self.assertIs(self.symbol, after_subs)
+
+    def test_substitute_proper_symbol(self):
+        after_subs = self.symbol.substitute(self.symbol, self.value)
+
+        self.assertIsNot(self.symbol, after_subs)
+        self.assertEqual(self.int_value, after_subs.get_value())
+
+    def test_get_symbols(self):
+        expected = [self.symbol]
+
+        actual = self.symbol.get_symbols()
+
+        self.assertCountEqual(expected, actual)
+
+    def test_get_value(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.symbol.get_value
+        )
+
+    def test_eq_comparison_with_self(self):
+        expected = True
+
+        actual = self.symbol == self.symbol
+
+        self.assertEqual(expected, actual)
+
+    def test_eq_comparison_with_different_symbol(self):
+        expected = False
+
+        actual = self.symbol == self.diff_symbol
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_symbolic_object/test_symbolic_object.py
@@ -0,0 +1,131 @@
+import unittest
+
+from neads.symbolic_objects import Symbol, Value, SymbolicObjectException, \
+    ListObject
+
+
+class TestSymbolicObject(unittest.TestCase):
+    """Test implementation of error checking in substitute on ListObject."""
+
+    def setUp(self) -> None:
+        self.symbol_1 = Symbol()
+        self.symbol_2 = Symbol()
+        self.value_1 = Value(1)
+        self.value_2 = Value(2)
+
+        self.test_object = ListObject(self.symbol_1, self.symbol_2)
+
+    def test_substitute_3_args(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            self.symbol_1,
+            self.value_1,
+            self.value_1
+        )
+
+    def test_substitute_2_args_first_not_symbol(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            self.value_1,
+            self.value_2
+        )
+
+    def test_substitute_2_args_second_not_symbolic_object(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            self.symbol_1,
+            'not_symbolic_object'
+        )
+
+    def test_substitute_1_args_not_dict_not_iterable(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            0
+        )
+
+    def test_substitute_1_args_dict_not_symbol_as_key(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            {self.value_1: self.value_2}
+        )
+
+    def test_substitute_1_args_dict_not_symbolic_object_as_value(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            {self.symbol_1: 'not_symbolic_object'}
+        )
+
+    def test_substitute_1_args_iterable_not_pairs(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            [self.symbol_1, self.value_1]
+        )
+
+    def test_substitute_1_args_iterable_not_symbol_in_pair(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            [(self.value_1, self.value_2)]
+        )
+
+    def test_substitute_1_args_iterable_not_symbolic_object_in_pair(self):
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            [(self.symbol_1, 'not_symbolic_object')]
+        )
+
+    def test_substitute_1_args_iterable_more_values_for_one_symbol(self):
+        subs_iterable = [
+            (self.symbol_1, self.value_1),
+            (self.symbol_1, self.value_2)
+        ]
+
+        self.assertRaises(
+            SymbolicObjectException,
+            self.test_object.substitute,
+            subs_iterable
+        )
+
+    def test_substitute_2_args_ok(self):
+        one_subs = self.test_object.substitute(self.symbol_1, self.value_1)
+        two_subs = one_subs.substitute(self.symbol_2, self.value_1)
+        result = two_subs.get_value()
+
+        expected = [1, 2]
+        self.assertEqual(expected, result)
+
+    def test_substitute_1_args_dict_ok(self):
+        subs_dict = {
+            self.symbol_1: self.value_1,
+            self.symbol_2: self.value_2
+        }
+
+        after_subs = self.test_object.substitute(subs_dict)
+        result = after_subs.get_value()
+
+        expected = [1, 2]
+        self.assertEqual(expected, result)
+
+    def test_substitute_1_args_iterable_ok(self):
+        subs_iterable = [
+            (self.symbol_1, self.value_1),
+            (self.symbol_2, self.value_2)
+        ]
+
+        after_subs = self.test_object.substitute(subs_iterable)
+        result = after_subs.get_value()
+
+        expected = [1, 2]
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
