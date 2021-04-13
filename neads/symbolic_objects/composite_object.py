7c44467ef64528026fda3d0585c288b1692a0b8a
Tomáš <hons.t.m@seznam.cz>
2021-04-13 15:39:08 +0200

    Implement CompositeObject, add common check for necessity subs

--- neads/symbolic_objects/composite_object.py
+++ neads/symbolic_objects/composite_object.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from typing import Iterable
 import abc
+import itertools
 
 from neads.symbolic_objects.symbolic_object import SymbolicObject, Symbol
 
@@ -7,8 +10,7 @@ from neads.symbolic_objects.symbolic_object import SymbolicObject, Symbol
 class CompositeObject(SymbolicObject):
     """Subtype of SymbolicObject combining several sub-objects."""
 
-    @abc.abstractmethod
-    def _substitute_clean(self, substitution_pairs) -> SymbolicObject:
+    def _substitute_clean(self, substitution_pairs) -> CompositeObject:
         """Do the substitution with iterable of pairs for substitution.
 
         If a replacement occurs, new CompositeObject is created from `self`,
@@ -24,6 +26,51 @@ class CompositeObject(SymbolicObject):
             CompositeObject after substitution.
         """
 
+        if self._is_affected_by_substitution(substitution_pairs):
+            return self._perform_substitution(substitution_pairs)
+        else:
+            return self
+
+    def _is_affected_by_substitution(self, substitution_pairs) -> bool:
+        """Return whether the object is affected by substitution.
+
+        Whether the object is affected by substitution, i.e. one of
+        `symbol_from` symbols in `substitution_pairs` is a Symbol in the
+        object.
+
+        Parameters
+        ----------
+        substitution_pairs
+            Iterable of pairs `symbol_from`, `object_to` for substitution.
+
+        Returns
+        -------
+            Whether the object is affected by substitution.
+        """
+
+        self_symbols = set(self.get_symbols())
+        symbol_to_substitute = set(symbol for symbol, object
+                                   in substitution_pairs)
+        common_symbols = self_symbols.intersection(symbol_to_substitute)
+        return len(common_symbols) > 0
+
+    @abc.abstractmethod
+    def _perform_substitution(self, substitution_pairs) -> CompositeObject:
+        """Actually perform substitution.
+
+        Create copy of self with sub-objects after substitution (i.e.
+        potentially new instances).
+
+        Parameters
+        ----------
+        substitution_pairs
+            Iterable of pairs `symbol_from`, `object_to` for substitution.
+
+        Returns
+        -------
+            Copy of self with sub-objects after substitution.
+        """
+
         pass
 
     def get_symbols(self) -> Iterable[Symbol]:
@@ -34,12 +81,10 @@ class CompositeObject(SymbolicObject):
             A list of all symbols which occur in the CompositeArgument.
         """
 
-        raise NotImplementedError()
-
-        # subarguments = self._get_sub_arguments()
-        # symbols_2d = [sub_arg.get_symbols() for sub_arg in subarguments]
-        # symbols_iter = itertools.chain(*symbols_2d)
-        # return set(symbols_iter)
+        subarguments = self._get_subobjects()
+        symbols_2d = [sub_arg.get_symbols() for sub_arg in subarguments]
+        symbols_iter = itertools.chain(*symbols_2d)
+        return set(symbols_iter)
 
     @abc.abstractmethod
     def get_value(self):
@@ -77,7 +122,7 @@ class CompositeObject(SymbolicObject):
         pass
 
     @abc.abstractmethod
-    def _get_sub_arguments(self) -> Iterable[SymbolicObject]:
+    def _get_subobjects(self) -> Iterable[SymbolicObject]:
         """Return an iterable of sub-objects which occur in the object.
 
         Returns
