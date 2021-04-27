d50b73f869e9af95cfeca9692e84cde05cd46575
Tomáš <hons.t.m@seznam.cz>
2021-04-27 11:16:21 +0200

    Change import style in symbolic_objects' __init__.py

--- neads/activation_model/symbolic_objects/__init__.py
+++ neads/activation_model/symbolic_objects/__init__.py
@@ -2,4 +2,4 @@ from .symbolic_object import Symbol
 from .value import Value
 from .symbolic_object_exception import SymbolicObjectException
 
-from .concrete_composite_objects import ListObject, DictObject
+from .concrete_composite_objects import *
