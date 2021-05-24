0f57bf192c1a2ef224a79981970c9f3b9d1f6460
Tomáš <hons.t.m@seznam.cz>
2021-05-24 14:30:35 +0200

    Add imports to activation_model's __init__

---
+++ neads/activation_model/__init__.py
@@ -0,0 +1,5 @@
+from symbolic_objects import *
+from activation_graph import Activation, ActivationGraph, SealedActivation, \
+    SealedActivationGraph
+from symbolic_argument_set import SymbolicArgumentSet
+from data_definition import DataDefinition
