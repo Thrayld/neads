d374fd1e50b08c782faabc1a24d173764c12b43c
Tomáš <hons.t.m@seznam.cz>
2021-06-18 18:27:10 +0200

    Add tests for DataNode

--- tests/test_evaluation_manager/test_single_thread_evaluation_manager/test_data_node.py
+++ tests/test_evaluation_manager/test_single_thread_evaluation_manager/test_data_node.py
@@ -1,4 +1,8 @@
 import unittest
+import unittest.mock as mock
+
+import pympler.asizeof
+from parameterized import parameterized
 
 from neads.evaluation_manager.single_thread_evaluation_manager.data_node \
     import DataNode, DataNodeStateException, DataNodeState
@@ -12,10 +16,13 @@ class TestDataNodeSingleNode(unittest.TestCase):
 
     def setUp(self) -> None:
         ag = SealedActivationGraph()
-        db = MockDatabase({})
+        self.value = 10
+        self.act = ag.add_activation(ar_plugins.const, self.value)
 
-        self.act = ag.add_activation(ar_plugins.const, 10)
-        self.dn = DataNode(self.act, [], db)
+        self.db = MockDatabase()
+        self.dn = DataNode(self.act, [], self.db)
+
+        self.callback_mock = mock.Mock()
 
     def test_try_load_with_not_unknown(self):
         assert not self.dn.try_load()
@@ -44,57 +51,198 @@ class TestDataNodeSingleNode(unittest.TestCase):
         )
 
     def test_transition_unknown_to_no_data(self):
-        pass
+        self.dn.register_callback_unknown_to_no_data(self.callback_mock)
+
+        assert not self.dn.try_load()
+
+        self.callback_mock.assert_called()
+        self.assertEqual(DataNodeState.NO_DATA, self.dn.state)
 
     def test_transition_unknown_to_memory(self):
-        pass
+        self.dn.register_callback_unknown_to_memory(self.callback_mock)
+        self.db.save(self.value, self.act.definition)
+
+        assert self.dn.try_load()
+
+        self.callback_mock.assert_called()
+        self.assertEqual(DataNodeState.MEMORY, self.dn.state)
 
     def test_transition_no_data_to_memory(self):
-        pass
+        self.dn.register_callback_no_data_to_memory(self.callback_mock)
+        assert not self.dn.try_load()
+
+        self.dn.evaluate()
+
+        self.callback_mock.assert_called()
+        self.assertEqual(DataNodeState.MEMORY, self.dn.state)
 
     def test_transition_memory_to_disk(self):
-        pass
+        self.dn.register_callback_memory_to_disk(self.callback_mock)
+        assert not self.dn.try_load()
+        self.dn.evaluate()
+
+        self.dn.store()
+
+        self.callback_mock.assert_called()
+        self.assertEqual(DataNodeState.DISK, self.dn.state)
 
     def test_transition_disk_to_memory(self):
-        pass
+        self.dn.register_callback_disk_to_memory(self.callback_mock)
+        assert not self.dn.try_load()
+        self.dn.evaluate()
+        self.dn.store()
 
-    def test_data_size(self):
-        pass
+        self.dn.load()
+
+        self.callback_mock.assert_called()
+        self.assertEqual(DataNodeState.MEMORY, self.dn.state)
+
+    def test_different_callback_not_called(self):
+        self.dn.register_callback_unknown_to_memory(self.callback_mock)
+
+        assert not self.dn.try_load()
+
+        self.callback_mock.assert_not_called()
+
+    def test_data_size_before_having_data(self):
+        self.assertEqual(None, self.dn.data_size)
+
+    def test_data_size_with_data(self):
+        self.dn.try_load()
+        self.dn.evaluate()
+
+        expected_size = pympler.asizeof.asizeof(self.value)
+        self.assertEqual(expected_size, self.dn.data_size)
 
 
 class TestDataNodeInGraph(unittest.TestCase):
 
-    def test_parents(self):
-        pass
+    @staticmethod
+    def _get_activation_graph():
+        ag = SealedActivationGraph()
+        act_1 = ag.add_activation(ar_plugins.const, 0)
+        act_2 = ag.add_activation(ar_plugins.add, act_1, 20)
+        act_3 = ag.add_activation(ar_plugins.add, act_1, 10)
+        act_4 = ag.add_activation(ar_plugins.sub, act_2, act_3)
 
-    def test_children(self):
-        pass
+        act_1.trigger_on_descendants = mock.Mock()
+        act_4.trigger_on_result = mock.Mock()
+        return ag, [act_1, act_2, act_3, act_4]
 
-    def test_level(self):
-        pass
+    @staticmethod
+    def _get_data_node_graph(ag, db):
+        act_to_dn = {}
+        dn_to_act = {}
+        for act in sorted(ag, key=lambda act: act.level):
+            parents = [act_to_dn[p] for p in act.parents]
+            new_dn = DataNode(act, parents, db)
+            act_to_dn[act] = new_dn
+            dn_to_act[new_dn] = act
+        return act_to_dn, dn_to_act
 
-    def test_has_trigger_on_result(self):
-        pass
+    def setUp(self) -> None:
+        self.ag, self.acts = self._get_activation_graph()
+        self.db = MockDatabase()
+        self.act_to_dn, self.dn_to_act \
+            = self._get_data_node_graph(self.ag, self.db)
+        self.dns = [self.act_to_dn[act] for act in self.acts]
 
-    def test_has_trigger_on_descendants(self):
-        pass
+    @parameterized.expand([
+        (0, []),
+        (1, [0]),
+        (2, [0]),
+        (3, [1, 2]),
+    ])
+    def test_parents(self, node_idx, expected_parents_idxs):
+        dn = self.dns[node_idx]
 
-    # def test_evaluate_with_parent_not_memory(self):
-    #     dn = self.es.get_top_level()[0]
-    #     assert not dn.try_load()
-    #     dn.evaluate()
-    #     dn.store()
-    #     assert dn.state is DataNodeState.DISK
-    #
-    #     child = dn.children[0]
-    #     assert not child.try_load()
-    #     assert child.state is DataNodeState.NO_DATA
-    #
-    #     self.assertRaises(
-    #         RuntimeError,
-    #         child.evaluate
-    #     )
+        actual_parent_idxs = [self.acts.index(self.dn_to_act[parent])
+                              for parent in dn.parents]
 
+        self.assertEqual(expected_parents_idxs, actual_parent_idxs)
+
+    @parameterized.expand([
+        (0, [1, 2]),
+        (1, [3]),
+        (2, [3]),
+        (3, []),
+    ])
+    def test_children(self, node_idx, expected_children_idxs):
+        dn = self.dns[node_idx]
+
+        actual_children_idxs = [self.acts.index(self.dn_to_act[child])
+                                for child in dn.children]
+
+        self.assertEqual(expected_children_idxs, actual_children_idxs)
+
+    @parameterized.expand([
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+    ])
+    def test_level(self, node_idx, expected_level):
+        dn = self.dns[node_idx]
+
+        actual_level = dn.level
+
+        self.assertEqual(expected_level, actual_level)
+
+    @parameterized.expand([
+        (0, False),
+        (1, False),
+        (2, False),
+        (3, True),
+    ])
+    def test_has_trigger_on_result(self, node_idx, expected_value):
+        dn = self.dns[node_idx]
+
+        actual_value = dn.has_trigger_on_result
+
+        self.assertEqual(expected_value, actual_value)
+
+    @parameterized.expand([
+        (0, True),
+        (1, False),
+        (2, False),
+        (3, False),
+    ])
+    def test_has_trigger_on_descendants(self, node_idx, expected_value):
+        dn = self.act_to_dn[self.acts[node_idx]]
+
+        actual_value = dn.has_trigger_on_descendants
+
+        self.assertEqual(expected_value, actual_value)
+
+    def test_evaluate_with_parents(self):
+        parent = self.dns[0]
+        child = self.dns[1]
+
+        parent.try_load()
+        parent.evaluate()
+        child.try_load()
+
+        child.evaluate()
+
+        self.assertEqual(DataNodeState.MEMORY, child.state)
+
+    def test_evaluate_with_parents_not_in_memory(self):
+        parent = self.dns[0]
+        child = self.dns[1]
+
+        parent.try_load()
+        parent.evaluate()
+        parent.store()
+        assert parent.state is DataNodeState.DISK
+
+        child.try_load()
+        assert child.state is DataNodeState.NO_DATA
+
+        # The actual test
+        self.assertRaises(
+            RuntimeError,
+            child.evaluate
+        )
 
 if __name__ == '__main__':
     unittest.main()
