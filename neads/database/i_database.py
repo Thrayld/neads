9f785ea7420721942ca77141218553bfb6b83d89
Tomáš <hons.t.m@seznam.cz>
2021-06-18 15:27:08 +0200

    Make IDatabase an abstract class

--- neads/database/i_database.py
+++ neads/database/i_database.py
@@ -1,26 +1,35 @@
+import abc
+
+
 class DataNotFound(Exception):
     pass
 
 
-class IDatabase:
+class IDatabase(abc.ABC):
     # TODO: should be implement enter, exit pattern? .. yes
 
     # TODO: also add finalizer (via weakref)
 
+    @abc.abstractmethod
     def __enter__(self):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def __exit__(self):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def open(self):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def close(self):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def save(self, data, data_definition):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def load(self, data_definition):
         raise NotImplementedError()
