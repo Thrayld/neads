b4d140c8c4fcd544e5683c55a7519dbefa910149
Tomáš <hons.t.m@seznam.cz>
2021-06-19 15:38:00 +0200

    Change TODO to better formulation

--- neads/utils/object_temp_file.py
+++ neads/utils/object_temp_file.py
@@ -7,8 +7,9 @@ from neads.utils.serializers.i_iserializer import ISerializer
 from neads.utils.serializers.pickle_serializer import PickleSerializer
 
 
-# TODO: Add ObjectTempFileCreator class with instance attribute 'path_generator'
-#  to prevent the risk of state sharing, i.e. PATH_GENERATOR in ObjectTempFile
+# TODO: Add IObjectTempFileProvider class which will create IObjectTempFile
+#  - easier mocking (which is very much desirable)
+#  - avoiding share state `PATH_GENERATOR`
 
 def _get_temp_path():
     """Generates a path for temporary file using tempfile library.
