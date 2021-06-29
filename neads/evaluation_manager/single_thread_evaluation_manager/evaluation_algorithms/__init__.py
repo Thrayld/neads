9dbf820f4b062581bba13c307ee3ff60aff4c1b0
Tomáš <hons.t.m@seznam.cz>
2021-06-29 16:31:16 +0200

    Add some imports for EA's package

---
+++ neads/evaluation_manager/single_thread_evaluation_manager/evaluation_algorithms/__init__.py
@@ -0,0 +1,5 @@
+from neads.evaluation_manager.single_thread_evaluation_manager \
+    .evaluation_algorithms.i_evaluation_algorithm import IEvaluationAlgorithm
+from neads.evaluation_manager.single_thread_evaluation_manager \
+    .evaluation_algorithms.topological_order_algorithm import \
+    TopologicalOrderAlgorithm
