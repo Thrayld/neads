50471b4190ae25d45071d314fbb735842f9e69be
Tomáš <hons.t.m@seznam.cz>
2021-06-29 16:30:48 +0200

    Implement TopologicalOrderAlgorithm

--- neads/evaluation_manager/single_thread_evaluation_manager/evaluation_algorithms/topological_order_algorithm.py
+++ neads/evaluation_manager/single_thread_evaluation_manager/evaluation_algorithms/topological_order_algorithm.py
@@ -4,11 +4,16 @@ from typing import TYPE_CHECKING, Any
 
 from neads.evaluation_manager.single_thread_evaluation_manager\
     .evaluation_algorithms.i_evaluation_algorithm import IEvaluationAlgorithm
+from neads.evaluation_manager.single_thread_evaluation_manager.data_node \
+    import DataNodeState
+
 
 if TYPE_CHECKING:
     from neads.activation_model import SealedActivation
     from neads.evaluation_manager.single_thread_evaluation_manager \
         .evaluation_state import EvaluationState
+    from neads.evaluation_manager.single_thread_evaluation_manager.data_node \
+        import DataNode
 
 
 class TopologicalOrderAlgorithm(IEvaluationAlgorithm):
@@ -42,4 +47,39 @@ class TopologicalOrderAlgorithm(IEvaluationAlgorithm):
             results.
         """
 
-        raise NotImplementedError()
+        while node := self._get_next_node_to_process(evaluation_state):
+            self._process(node)
+
+        results = {
+            node.activation: node.get_data()
+            for node in evaluation_state.results
+        }
+
+        return results
+
+    def _get_next_node_to_process(self, evaluation_state: EvaluationState):
+        """Next node to process.
+
+        Returns
+        -------
+            Next node to process, i.e. the top most unprocessed node. None,
+            if all nodes were processed.
+        """
+
+        unprocessed_nodes = [node for node in evaluation_state
+                             if self._is_unprocessed(node)]
+        if unprocessed_nodes:
+            sorted_unprocessed_nodes = sorted(unprocessed_nodes,
+                                              key=lambda dn: dn.level)
+            return sorted_unprocessed_nodes[0]
+        else:
+            return None
+
+    @staticmethod
+    def _is_unprocessed(data_node: DataNode):
+        return data_node.state is DataNodeState.UNKNOWN
+
+    @staticmethod
+    def _process(node: DataNode):
+        if not node.try_load():
+            node.evaluate()
