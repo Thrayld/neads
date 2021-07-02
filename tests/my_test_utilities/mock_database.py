a8888f5a68791ed432ecb997a41902247a3cc01f
Tomáš <hons.t.m@seznam.cz>
2021-07-02 16:49:50 +0200

    Refactor MockDatabase so it comply with IDatabase

--- tests/my_test_utilities/mock_database.py
+++ tests/my_test_utilities/mock_database.py
@@ -6,23 +6,28 @@ from neads.activation_model import DataDefinition
 
 class MockDatabase(IDatabase):
 
+    @property
+    def is_open(self):
+        return self._is_open
+
+    def _do_open(self):
+        self._is_open = True
+
+    def _do_close(self):
+        self._is_open = False
+
+    def _do_save(self, data, key):
+        self._content[key] = data
+
+    def _do_load(self, key):
+        try:
+            return self._content[key]
+        except KeyError as e:
+            raise DataNotFound() from e
+
     def __init__(self,
                  database_content: Optional[dict[DataDefinition, Any]] = None):
+        self._is_open = False
         if database_content is None:
             database_content = {}
         self._content = database_content
-
-    def open(self):
-        pass
-
-    def close(self):
-        pass
-
-    def save(self, data, data_def):
-        self._content[data_def] = data
-
-    def load(self, data_def):
-        try:
-            return self._content[data_def]
-        except KeyError as e:
-            raise DataNotFound() from e
