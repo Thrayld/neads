ee6a2d74017be85ab0b58c0e83cdd9416c5d5fa9
Tomáš <hons.t.m@seznam.cz>
2021-07-02 14:11:50 +0200

    Add CA's simple test

---
+++ tests/test_evaluation_manager/test_single_thread_evaluation_manager/test_evaluation_algorithms/test_complex_algorithm.py
@@ -0,0 +1,13 @@
+from tests.test_evaluation_manager.test_single_thread_evaluation_manager \
+    .test_evaluation_algorithms.test_evaluation_algorithm import \
+    BaseTestClassWrapper
+from neads.evaluation_manager.single_thread_evaluation_manager \
+    .evaluation_algorithms import ComplexAlgorithm
+
+
+class TestComplexAlgorithmWithoutSwapping(BaseTestClassWrapper.
+                                          BaseTestEvaluationAlgorithm):
+    """Tests serializer based on the pickle protocol."""
+
+    def get_algorithm(self):
+        return ComplexAlgorithm()
