61de8ae9204987feaab68538b74f9decee7efb38
Tomáš <hons.t.m@seznam.cz>
2021-07-04 15:27:36 +0200

    Add assert_graph_has_no_triggers method

--- neads/utils/graph_utils.py
+++ neads/utils/graph_utils.py
@@ -29,3 +29,27 @@ def get_result_activation(graph: ActivationGraph):
             f'The graph does not have one result Activation: {result_acts}'
         )
 
+
+def assert_graph_has_no_triggers(graph: ActivationGraph):
+    """Assert that the graph has no trigger (graph's nor Activations').
+
+    Parameters
+    ----------
+    graph
+        The checked graph.
+
+    Raises
+    ------
+    ValueError
+        If the graph or one of its Activations has a trigger method.
+    """
+
+    raise NotImplementedError()
+    # has_graph_trigger = bool(graph.trigger_method)
+    # has_trigger_on_result = bool(act for act in graph
+    #                              if act.trigger_on_result)
+    # has_trigger_on_descendants = bool(act for act in graph
+    #                                   if act.trigger_on_descendants)
+    #
+    # if has_graph_trigger or has_trigger_on_result or has_trigger_on_descendants:
+    #     raise ValueError('The graph contains a trigger method')
