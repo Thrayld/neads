c264851ceef962b8ca0775613b4340b2ec733380
Tomáš <hons.t.m@seznam.cz>
2021-07-04 15:27:59 +0200

    Add tests for assert_graph_has_no_triggers method

--- tests/test_utils/test_graph_utils.py
+++ tests/test_utils/test_graph_utils.py
@@ -6,7 +6,7 @@ from neads.activation_model import ActivationGraph
 import tests.my_test_utilities.arithmetic_plugins as ar_plugins
 
 
-class TestGraphUtils(unittest.TestCase):
+class TestGetResultActivation(unittest.TestCase):
     def test_get_result_activation_different_number_of_result_act(self):
         ag = ActivationGraph(2)
         ag.add_activation(ar_plugins.const, ag.inputs[0])
@@ -26,6 +26,48 @@ class TestGraphUtils(unittest.TestCase):
         actual = graph_utils.get_result_activation(ag)
 
         self.assertIs(act_2, actual)
+
+
+class TestAssertGraphHasNotTriggers(unittest.TestCase):
+    def test_assert_graph_has_no_triggers_with_graph_trigger(self):
+        ag = ActivationGraph(2)
+        ag.add_activation(ar_plugins.const, ag.inputs[0])
+        ag.trigger_method = lambda: None
+
+        self.assertRaises(
+            ValueError,
+            graph_utils.assert_graph_has_no_triggers,
+            ag
+        )
+
+    def test_assert_graph_has_no_triggers_with_trigger_on_result(self):
+        ag = ActivationGraph(2)
+        act_1 = ag.add_activation(ar_plugins.const, ag.inputs[0])
+        act_1.trigger_on_result = lambda _: None
+
+        self.assertRaises(
+            ValueError,
+            graph_utils.assert_graph_has_no_triggers,
+            ag
+        )
+
+    def test_assert_graph_has_no_triggers_with_trigger_on_descendants(self):
+        ag = ActivationGraph(2)
+        act_1 = ag.add_activation(ar_plugins.const, ag.inputs[0])
+        act_1.trigger_on_descendants = lambda: None
+
+        self.assertRaises(
+            ValueError,
+            graph_utils.assert_graph_has_no_triggers,
+            ag
+        )
+
+    def test_assert_graph_has_no_triggers_without_triggers(self):
+        ag = ActivationGraph(2)
+        ag.add_activation(ar_plugins.const, ag.inputs[0])
+
+        graph_utils.assert_graph_has_no_triggers(ag)
+
 
 if __name__ == '__main__':
     unittest.main()
