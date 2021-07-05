421ae03b8698ea987b0a0dbe3b46e0f4299bdbf9
Tomáš <hons.t.m@seznam.cz>
2021-07-05 14:56:03 +0200

    Add directory for ChoicesStep

--- neads/sequential_choices_model/choices_step/choices_step.py
+++ neads/sequential_choices_model/choices_step/choices_step.py
@@ -1,5 +1,11 @@
-from neads.activation_model import SealedActivationGraph, SealedActivation
-from neads.sequential_choices_model.i_step import IStep
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from neads.activation_model import SealedActivationGraph, SealedActivation
+    from neads.sequential_choices_model.i_step import IStep
+    from neads.sequential_choices_model.tree_view import TreeView
 
 
 class ChoicesStep(IStep):
@@ -13,10 +19,12 @@ class ChoicesStep(IStep):
         """Initialize empty ChoicesStep."""
 
         raise NotImplementedError()
+
         # self.choices = []
 
     def create(self, target_graph: SealedActivationGraph,
-               parent_activation: SealedActivation, tree_view,
+               parent_activation: SealedActivation,
+               tree_view: TreeView,
                next_steps: list[IStep]):
         """Add each choice to the graph and recursively adds the next steps.
 
@@ -37,3 +45,17 @@ class ChoicesStep(IStep):
         """
 
         raise NotImplementedError()
+
+        # # Creating the step's part of the graph
+        # step_results = []
+        # for choice in self.choices:
+        #     result_act = choice.attach(target_graph, parent_activation)
+        #     tree_view.add_child(parent_activation, result_act)
+        #     step_results.append(result_act)
+        # # Invoking next steps to create their part of the graph
+        # if next_steps:
+        #     next_step = next_steps[0]
+        #     following_steps = next_steps[1:]
+        #     for result_act in step_results:
+        #         next_step.create(target_graph, result_act, tree_view,
+        #                          following_steps)
