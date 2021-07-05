18abcf016debe50d272a5ccca63e8dffb9518cbb
Tomáš <hons.t.m@seznam.cz>
2021-07-05 17:01:29 +0200

    Add API of Extractor

---
+++ neads/sequential_choices_model/dynamic_step/extractor.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from neads.activation_model import ActivationGraph, SealedActivation, \
+        SealedActivationGraph
+
+
+class Extractor:
+    """Part of dynamic step, process the subtasks produces by separator.
+
+    The extractor is an ActivationGraph with 3 inputs, the first for the data
+    of the Activation which served as input to the separator (i.e. the
+    `parent_activation` in create method), the second for data of separator
+    and the last for index to the data. The extractor has, as usual,
+    one result Activation which is considered to be the result of step's
+    choice. That is, step's choices results are the result Activation of
+    attached extractors (i.e. they are added as children of the
+    `parent_activation` to the TreeView and the next steps are attached to
+    them).
+    """
+
+    def __init__(self, graph: ActivationGraph):
+        """Initialize extractor with its graph.
+
+        Parameters
+        ----------
+        graph
+            The graph which represent the operations of the extractor.
+            It has three inputs and one result Activation (i.e. single childless
+            Activation) and does not have a trigger method (nor its
+            Activations).
+            See docstrings of the class and the `attach` method for more on
+            the role of each extractor's input.
+
+        Raises
+        ------
+        ValueError
+            If the graph does not have three inputs and one result Activation.
+            If the graph has a trigger method.
+        """
+
+        raise NotImplementedError()
+
+    def attach(self, target_graph: SealedActivationGraph,
+               data_activation: SealedActivation,
+               instruction_activation: SealedActivation,
+               instruction_index: int):
+        """Attach the extractor's graph to the given graph.
+
+        Parameters
+        ----------
+        target_graph
+            The graph to which will be the extractor's graph attached.
+        data_activation
+            The Activation which serves as a data source for the extractor.
+            That is, the Activation to which the separator was attached.
+            This parameter is realization of the first input of the
+            extractor's graph.
+        instruction_activation
+            The Activation which serves as a instruction source for the
+            extractor. That is, the separator's result Activation.
+            This parameter is realization of the second input of the
+            extractor's graph.
+        instruction_index
+            Index of the instruction designated for the attached copy
+            of extractor. This parameter is realization of the third input of
+            the extractor's graph.
+
+        Returns
+        -------
+            The Activation of target graph, to which is mapped the result
+            Activation of the extractor's graph.
+        """
+
+        raise NotImplementedError()
