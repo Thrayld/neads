af3928bce19467bdeea1698b37156f206a18c806
Tomáš <hons.t.m@seznam.cz>
2021-07-07 15:20:11 +0200

    Add new arithmetic plugin
    The plugin returns a sequence. Such plugins are
    necessary for mocking of DynamicStep's separator.

--- tests/my_test_utilities/arithmetic_plugins.py
+++ tests/my_test_utilities/arithmetic_plugins.py
@@ -37,6 +37,23 @@ def _pow(x, base=2):
     return base**x
 
 
+def _factor(n):
+    """Return ordered list primes which divide the given number n."""
+    sieve = [True] * n
+    sieve[0] = False
+    factors = []
+    idx = 1
+    while idx < len(sieve):
+        if sieve[idx]:
+            examined = idx + 1
+            if not n % examined:
+                factors.append(examined)
+                for remove_idx in range(idx + examined, len(sieve), examined):
+                    sieve[remove_idx] = False
+        idx += 1
+    return factors
+
+
 const = Plugin(PluginID('const', 0), _const)
 add = Plugin(PluginID('add', 0), _add)
 sub = Plugin(PluginID('sub', 0), _sub)
@@ -45,3 +62,4 @@ div = Plugin(PluginID('div', 0), _div)
 max = Plugin(PluginID('max', 0), _max)
 min = Plugin(PluginID('min', 0), _min)
 pow = Plugin(PluginID('pow', 0), _pow)
+factor = Plugin(PluginID('factor', 0), _factor)
