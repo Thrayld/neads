e0d367c950827c9e60e2b23aaa4aa8ea77823236
Tomáš <hons.t.m@seznam.cz>
2021-07-08 11:47:57 +0200

    Add Plugin and PluginID to main __init__.py
    For the user who wants to create a custom plugins.

--- neads/__init__.py
+++ neads/__init__.py
@@ -2,3 +2,4 @@ from neads.activation_model import ActivationGraph, SealedActivationGraph, \
     Value, ListObject, DictObject
 from neads.sequential_choices_model import SequentialChoicesModel, \
     ChoicesStep, DynamicStep, Choice, Separator, Extractor
+from neads.plugin import Plugin, PluginID
