4565f292a50bb3eb2e1fd275d12e5ee5ad219399
Tomáš <hons.t.m@seznam.cz>
2021-07-19 17:06:52 +0200

    Fix __example_03.py

--- examples/__example_03.py
+++ examples/__example_03.py
@@ -1,7 +1,7 @@
 import logging
 
 import neads as nd
-from examples import example_01
+from examples import example_03
 
 DATA_PATH = 'C:\\Users\\Honst\\BP\\evaluations\\my_df'
 DATABASE_PATH = 'C:\\Users\\Honst\\BP\\evaluations\\my_example_03'
@@ -11,7 +11,7 @@ nd.configure_logging(logging.DEBUG)
 nd.FileDatabase.create(DATABASE_PATH)
 db = nd.FileDatabase(DATABASE_PATH)
 
-scm = example_01.get_example(DATA_PATH)
+scm = example_03.get_example(DATA_PATH)
 manager = nd.SingleThreadEvaluationManager(db)
 results = manager.evaluate(scm.create_graph())
 
