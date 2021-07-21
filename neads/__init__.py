b1650d75e5203edee969d02915922987eb12d089
Tomáš <hons.t.m@seznam.cz>
2021-07-21 14:18:25 +0200

    Add ComplexAlgorithm to attributes of Neads pkg

--- neads/__init__.py
+++ neads/__init__.py
@@ -3,6 +3,7 @@ from neads.activation_model import ActivationGraph, SealedActivationGraph, \
 from neads.sequential_choices_model import SequentialChoicesModel, \
     ChoicesStep, DynamicStep, Choice, Separator, Extractor
 from neads.activation_model.plugin import Plugin, PluginID
-from neads.evaluation_manager import SingleThreadEvaluationManager
+from neads.evaluation_manager import SingleThreadEvaluationManager, \
+    ComplexAlgorithm
 from neads.database import FileDatabase
 from neads.logging_autoconfig import configure_logging
